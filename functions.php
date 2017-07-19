62a7d95fa89844ab23dd2cb7a1764253ef277d97
Mikhail Bocharov <mike.b.89@mail.ru>
2017-07-19 04:50:29 +0300

    1st commit

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 19.07.17
+ * Time: 4:46
+ */
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 19.07.17
+ * Time: 4:47
+ */
+
+require "functions.php";
