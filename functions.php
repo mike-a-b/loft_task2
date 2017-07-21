4f2d2cc87c58676e7b05bc3cd4050dd9e992b8bb
Mikhail Bocharov <mike.b.89@mail.ru>
2017-07-21 03:00:23 +0300

    add task2 and rev. task1

--- functions.php
+++ functions.php
@@ -6,31 +6,61 @@
  * Time: 4:46
  */
 
-function task1()
-{
-    $num_args = func_num_args();
-    $str_array = [];
-    if ($num_args>2) {
-        die();
-    } else {
-        $str_array = func_get_arg(0);
-    }
-    if (!is_array($str_array)) {
-        die();
-    }
+if (TASK1 === true) {
+    function task1()
+    {
+        $num_args = func_num_args();
+        $str_array = [];
+        if ($num_args > 2) {
+            die("Параметр 1й - массив строк, 2-й - логический");
+        } else {
+            $str_array = func_get_arg(0);
+        }
+        if (!is_array($str_array)) {
+            die("1й параметр некорректен");
+        }
 
-    if (func_get_arg(1) === true) {
-        $result = implode("", $str_array);
-        echo $result."<br>";
-        return;
-    } else {
-        foreach ($str_array as $str) {
-            echo "<p>$str</p>\n";
+        if (func_get_arg(1) === true) {
+            $result = implode("", $str_array);
+            echo $result . "<br>";
+            return;
+        } else {
+            foreach ($str_array as $str) {
+                echo "<p>$str</p>\r\n";
+            }
         }
     }
 }
 
-function task2()
-{
-    //
+if (TASK2 === true) {
+    function task2($int_array, $operation)
+    {
+        $num_args = func_num_args();
+        $int_ar = [];
+        $op = ['+', '-', '/', '*'];
+//    echo in_array($operation, $op);
+        if ($num_args !== 2) {
+            echo "Неверное количество элементов! (допустимо 2, передаваемых в функцию(1 - массив чисел, 2й " .
+                "- арифметическая операция)";
+            die();
+        } elseif (gettype($int_array) != "array") {
+            echo("Первый параметр доллжен быть массивом");
+            foreach ($int_array as $item) {
+                (is_integer($item)) ? true : die("Элементы массива должны быть именно числами");
+            }
+        } elseif (/*(strlen($operation) != 1) &&*/
+        (!in_array($operation, $op, true))
+        ) {
+            die("Некорректная арифметическая операция");
+        } else {
+            foreach ($int_array as $value) {
+                $str = "Выполняем $operation c элементом массива  $value : $value " . $operation . " $value\n";
+                echo $str;
+                $val = "$value $operation $value";
+//                $result = eval("return $val;");
+                $result = eval("return ($value $operation $value);");
+                echo '<br>Результат = ' . $result . '<br>';
+            }
+        }
+    }
 }
--- index.php
+++ index.php
@@ -5,8 +5,14 @@
  * Date: 19.07.17
  * Time: 4:47
  */
+define("TASK1", true);
+define("TASK2", true);
 
 require "functions.php";
 
-$ar = ["one", "two" , "three"];
-task1($ar, true);
+echo "<h2>Задание № 1</h2>";
+task1(["one", "two" , "three"], true);
+
+echo "<h2>Задание № 2</h2>";
+$arr = [1,8,3,4];
+task2($arr, '/');
