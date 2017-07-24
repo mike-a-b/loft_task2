dc867213f9398a6799c5f5eb8822bc87f586bf08
Mikhail Bocharov <mike.b.89@mail.ru>
2017-07-24 12:24:45 +0300

    end of task 10

--- index.php
+++ index.php
@@ -20,20 +20,20 @@ define("TASK10", true);
 
 require "functions.php";
 
-/*echo "<h2>Задание № 1</h2>";
-task1(["one", "two" , "three"], true);*/
+echo "<h2>Задание № 1</h2>";
+task1(["one", "two" , "three"], true);
 
-/*echo "<h2>Задание № 2</h2>";
+echo "<h2>Задание № 2</h2>";
 task2([1,8,3,4], '/');
 
 echo "<h2>Задание № 3</h2>";
-task3('/', 4, 2.2, 3, 6.5, 10);*/
+task3('/', 4, 2.2, 3, 6.5, 10);
 
-/*echo "<h2>Задание № 4</h2>";
+echo "<h2>Задание № 4</h2>";
 //multipl... table echo
-task4(4, 6);*/
+task4(4, 6);
 
-/*echo "<h2>Задание № 5</h2>";
+echo "<h2>Задание № 5</h2>";
 //полиндром в качестве примера...
 $st_pol = "У скал плакса ласкал плаксу";
 //НЕ полиндром из него же...
@@ -56,18 +56,18 @@ if ($result === true) {
     }
     $result = "not_poliecho";
     $result();
-}*/
+}
 
-/*echo "<h2>Задание № 6</h2>";
+echo "<h2>Задание № 6</h2>";
 
 date_default_timezone_set('Europe/Moscow');
-task6();*/
+task6();
 
-/*echo "<h2>Задание № 7</h2>";
+echo "<h2>Задание № 7</h2>";
 
-task7();*/
+task7();
 
-/*echo "<h2>Задание № 8</h2>";
+echo "<h2>Задание № 8</h2>";
 $receive_strs = [
     "RX packets:950381 errors:0 dropped:0 overruns:0 frame:0. ",
     'RX packets:300 errors:90 dropped:0 overruns:0 frame:0. ',
@@ -78,8 +78,8 @@ $receive_strs = [
     "RX packets:300 errors:90 dropped:0 overruns:0 frame:0. "
 ];
 
-task8($receive_strs);*/
+task8($receive_strs);
 
-//task9();
+task9();
 
 task10();
